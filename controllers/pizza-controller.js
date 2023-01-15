62e542980aaaad3e875ffe0e2b99044023d9a727
Darryl Johnson <dandycodes@gmail.com>
2023-01-15 18:18:40 -0500

    getPizzas method added

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -2,6 +2,16 @@ const {Pizza} = require('../models')
 
 const pizzaController = {
     // function and methods placed inside
+    // need methods to GET ALL pizzas and a single pizza by its
+    // id
+    getPizzas(req, res) {
+        Pizza.find({})
+        .then(pizzaData => res.json(pizzaData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err)
+        })
+    }
 }
 
 
