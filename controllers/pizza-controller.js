9cd051875a6f7083c4e4d6787fff6a7b252468a3
Darryl Johnson <dandycodes@gmail.com>
2023-01-15 18:26:08 -0500

    missing comma

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -11,6 +11,21 @@ const pizzaController = {
             console.log(err);
             res.status(400).json(err)
         })
+    },
+    // get a singular pizza 
+    getSinglePizza({params}, res) {
+        Pizza.findOne({_id: params.id})
+            .then(pizzaData => {
+                if(!pizzaData) {
+                    res.status(404).json({message: 'No pizza found with that id'})
+                    return
+                }
+                res.json(pizzaData)
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
     }
 }
 
