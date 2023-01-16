ea20da7f69d0179abf7426328ef89c27a7cb83f2
Darryl Johnson <dandycodes@gmail.com>
2023-01-15 19:06:21 -0500

    added api route folder

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const htmlRoutes = require('./html/html-routes');
-// added
+// add api route import below 
+const apiRoutes = require('./api')
+
 
 router.use('/', htmlRoutes);
+router.use('./api', apiRoutes)
 
 router.use((req, res) => {
   res.status(404).send('<h1>😝 404 Error!</h1>');
