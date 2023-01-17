8410ceb124a74483009748dabb20e90c5a089ff7
Darryl Johnson <dandycodes@gmail.com>
2023-01-17 11:48:32 -0500

    add comment method added

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,10 +1,15 @@
 const {Comment, Pizza} = require('../models')
 
 // need to add CRUD methods for comments
+// In this method comments belong to a Pizza and its id
 const commentController = {
 
-    addComment() {
-
+    addComment({params, body}, res) {
+        console.log(body)
+        Comment.create(body)
+            .then(({_id}) => {
+                console.log(_id)
+            })
     },
 
     deleteComment() {
