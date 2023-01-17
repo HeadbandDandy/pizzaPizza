cc8afc97ee69764e8a2592ead2d3d4eb1041454a
Darryl Johnson <dandycodes@gmail.com>
2023-01-17 11:46:37 -0500

    structure added need to insert logic

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -3,4 +3,15 @@ const {Comment, Pizza} = require('../models')
 // need to add CRUD methods for comments
 const commentController = {
 
+    addComment() {
+
+    },
+
+    deleteComment() {
+
+    }
+
 }
+
+
+module.exports = commentController;
